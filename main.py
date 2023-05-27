47eac003319a9e457e8c32f374835b5a260df623
dfpewsdokcdsv <132742180+dfpewsdokcdsv@users.noreply.github.com>
2023-05-27 15:45:10 +0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,31 @@
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Привет! Я бот {bot.user}!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+    
+    async def on_message(self, message):
+        if message.content.startswith('!deleteme'):
+            msg = await message.channel.send('I will delete myself now...')
+            await msg.delete()
+            await message.channel.send('Goodbye in 3 seconds...', delete_after=3.0)
+
+    async def on_message_delete(self, message):
+        msg = f'{message.author} has deleted the message: {message.content}'
+        await message.channel.send(msg)
+
+bot.run("token")
